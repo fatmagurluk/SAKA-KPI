a336562b399d4710ea99bd7f7a17857ed8f2af17
fatmagurluk <fatmagurluk@gmail.com>
2016-05-07 11:49:12 +0300

    servis katmanları ve sql bağlantı oluşturuldu

---
+++ SAKA.Service.Contract/IKPIService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace SAKA.Service.Contract
+{
+    [ServiceContract] // anlaşma kağıdım
+    public interface IKPIService // Business ' i imzalamak zorundayım
+    {
+        [OperationContract] // 
+        int count();
+    }
+}
---
+++ SAKA.Web.UI/Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ServiceModel;
+using SAKA.Service.Contract;
+
+namespace SAKA.Web.UI
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // ABC (Adres Binding Contract)
+
+            var adress = new EndpointAddress("http://localhost:52695/KPIService.svc"); // service bağlantısı kuruyorum (Contract)
+            var binding = new BasicHttpBinding();//Bağlantı binding yapıyorum
+            var channel = ChannelFactory<IKPIService>.CreateChannel(binding, adress); // channel elimde kodlar olduğu iiçn kullanıyorum. proxy elimde kodlar olmasaydı kullanırdım
+
+            //bunları yazdıktan sonra kodları kullanabilirim
+
+            var count = channel.count();
+
+            Response.Write(count);
+        }
+    }
+}
